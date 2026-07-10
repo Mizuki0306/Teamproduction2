206d023d61fa303cf87b67e77e50c0bf1a567f06
瑞希 桑原 <2247005@ecc.ac.jp>
2026-07-10 14:46:10 +0900

    Guideでの変数実装

--- Assets/Scripts/BrockGuide.cs
+++ Assets/Scripts/BrockGuide.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class BrockGuide : MonoBehaviour
 {
+    private Brock brockScript;
+    public enum GuideType { Push, Pull, PushandPull }
+    public GuideType guideType;
+    public int brockNum;
+    [SerializeField] private GameObject guide_Collider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        brockScript = FindFirstObjectByType<Brock>();
+        guide_Collider = transform.Find("GuideCollider").gameObject;
     }
 
     // Update is called once per frame
